ec96ba2ebda2a5874b029a99968b9fd3ee777590
luis <lvegerano422@gmail.com>
2014-11-06 17:29:42 -0600

    started tests

--- test/index.js
+++ test/index.js
@@ -2,19 +2,35 @@ var Lab = require('lab');
 var Hapi = require('hapi');
 var Hoek = require('hoek');
 var Code = require('code');
+var jwt = require('../');
 
 var internals = {};
 
-var expect = Lab.expect;
-var before = Lab.before;
-var describe = Lab.experiment;
-var it = Lab.test;
+// test shortcuts
+var lab = exports.lab = Lab.script();
+var expect = Code.expect;
+var before = lab.before;
+var describe = lab.describe;
+var it = lab.it;
 
-var server = new Hapi.Server();
-before(function(done) {
-    server.pack.register(require('../'), function(err) {
-        expect(err).to.not.exist;
-        server.auth.strategy()
+var privateKey = 'kljdf[4aoinfl!$!#$v3413$#234knd*()*skladjsfp86$%54andkjvyasd';
 
+describe('JWT', function() {
+    var token = jwt.sign({ foo: 'bar'}, privateKey);
+    //partial test token
+    it('should return a string', function(done) {
+        expect(token).to.be.a.string();
+        done();
     });
 });
+
+describe('JWT', function() {
+    var token = jwt.sign({ foo: 'bar'}, privateKey);
+    var decoded = jwt.decode(token);
+    //partial test token
+    it('should return an object', function(done) {
+        expect(decoded).to.be.an.object();
+        done();
+    });
+});
+
