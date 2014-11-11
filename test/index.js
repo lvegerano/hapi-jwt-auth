2787efd8a706bf6aa983d848570f51e46a18d688
luis <lvegerano422@gmail.com>
2014-11-11 17:45:09 -0600

    added new tests

--- test/index.js
+++ test/index.js
@@ -15,7 +15,7 @@ var it = lab.it;
 
 var privateKey = 'kljdf[4aoinfl!$!#$v3413$#234knd*()*skladjsfp86$%54andkjvyasd';
 
-describe('JWT', function() {
+describe('hapi-jwt', function() {
     var token = jwt.sign({ foo: 'bar'}, privateKey);
     //partial test token
     it('should return a string', function(done) {
@@ -24,13 +24,83 @@ describe('JWT', function() {
     });
 });
 
-describe('JWT', function() {
+describe('hapi-jwt', function() {
     var token = jwt.sign({ foo: 'bar'}, privateKey);
     var decoded = jwt.decode(token);
     //partial test token
     it('should return an object', function(done) {
         expect(decoded).to.be.an.object();
         done();
+    });
+});
+
+describe('hapi-jwt', function() {
+    var header = function(username, options) {
+        options = options || {};
+
+        return 'Bearer ' + jwt.sign({ username: username }, privateKey, options);
+    };
+
+    var validateUser = function(token, decoded, callback) {
+        var username = decoded.username;
+        switch (username) {
+            case 'luis':
+                return callback(null, true, { user: 'luis', scope: ['x'] });
+        }
+    };
+
+    var handler = function(req, rep) {
+        rep('ok');
+    };
+
+    var server = new Hapi.Server({ debug: false });
+    before(function(done) {
+        server.pack.register(require('../'), function(err) {
+            expect(err).to.not.exist();
+            server.auth.strategy('token', 'jwt', 'required', { validateFunc: validateUser });
+            server.route([
+                {
+                    method: 'POST',
+                    path: '/base',
+                    config: {
+                        handler: handler,
+                        auth: 'token'
+                    }
+                }
+            ]);
+            done();
+        });
+    });
+
+    it('should return a reply on successful token verification', function(done) {
+        var request = {
+            method: 'POST',
+            url: '/base',
+            headers: {
+                authorization: header('luis')
+            }
+        };
+        server.inject(request, function(res) {
+            expect(res.result).to.exist();
+            expect(res.result).to.equal('ok');
+            done();
+        });
+    });
+
+    it('should return a 401 code when using a bad scheme' , function(done) {
+        var request = {
+            method: 'POST',
+            url: '/base',
+            header: {
+                authorization: 'Something crazy'
+            }
+        };
+        server.inject(request, function(res) {
+            expect(res.result).to.exist();
+            expect(res.statusCode).to.be.a.number();
+            expect(res.statusCode).to.equal(401);
+            done();
+        });
     });
 });
 
